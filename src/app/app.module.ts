a12515b40e9a998ce937490db35e409aff53532b
Ahmad <a.ebeid>
2017-10-08 17:06:08 +0200

    doneWithInitialUpdateUserNotCompletedYet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http'
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { DatabaseInfoComponent } from './database-info/database-info.component';
 import { ProfileInputComponent } from './profile-input/profile-input.component';
 import { ProfileIdinfoComponent } from './profile-idinfo/profile-idinfo.component';
+
+import { ServerConnectionsService } from './server-connections.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +17,11 @@ import { ProfileIdinfoComponent } from './profile-idinfo/profile-idinfo.componen
     ProfileIdinfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ServerConnectionsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
